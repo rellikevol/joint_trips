5cc58b2116fde7c3105dac4ddba66f587236a902
rellikevol <rellikevols@gmail.com>
2023-07-13 16:31:08 +0600

    страницы в футере

--- example/urls.py
+++ example/urls.py
@@ -4,5 +4,9 @@ from example import views
 
 urlpatterns = [
     path('', views.index, name='index-page'),
-    path('how_it_works/', views.how_it_works, name='how-it-works')
+    path('how_it_works/', views.how_it_works, name='how-it-works'),
+    path('about_us/', views.about_us, name='about-us'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('terms_of_use/', views.terms_of_use, name='terms-of-use'),
+    path('privacy_policy/', views.privacy_policy, name='privacy-policy')
 ]
--- example/views.py
+++ example/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
+
+
 # Create your views here.
 
 
 def index(request):
     return render(request, 'example/main_page.html')
 
+
 def how_it_works(request):
     return render(request, 'example/how_it_works_page.html')
 
 
+def about_us(request):
+    return render(request, 'example/about_us_page.html')
 
+
+def contacts(request):
+    return render(request, 'example/contacts_page.html')
+
+
+def terms_of_use(request):
+    return render(request, 'example/terms_of_use.html')
+
+
+def privacy_policy(request):
+    return render(request, 'example/privacy_policy.html')
