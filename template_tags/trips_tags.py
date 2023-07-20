274e34662e803c9ae65da4e7a1b939fb5d9f8424
rellikevol <rellikevols@gmail.com>
2023-07-20 12:08:30 +0600

    сборка к 20.07.2023 тег яндекса

--- template_tags/trips_tags.py
+++ template_tags/trips_tags.py
@@ -1,12 +1,19 @@
 from django import template
 from trips.models import trip_animals, trip_smoke
 from users.models import profile_smoke, profile_animals, profile_music, profile_conversations
-
+import os
+from dotenv import load_dotenv
+from core.settings import BASE_DIR
 
 register = template.Library()
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+API_YANDEX_GEO = os.environ.get('API_YANDEX_GEO')
+
 trips_choice = trip_smoke + trip_animals
 users_choice = profile_smoke + profile_animals + profile_music + profile_conversations
+
 
 @register.filter
 def trip_tag(x):
@@ -15,9 +22,15 @@ def trip_tag(x):
             return choice[1]
     return ''
 
+
 @register.filter
 def users_tag(x):
     for choice in users_choice:
         if choice[0] == x:
             return choice[1]
     return ''
+
+
+@register.simple_tag
+def yandex_key():
+    return API_YANDEX_GEO
