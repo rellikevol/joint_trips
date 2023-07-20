f00b53a35b06895cb03b87af74214429bc41d58d
rellikevol <rellikevols@gmail.com>
2023-07-20 16:09:20 +0600

    final 0.1

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+from PIL import Image
 
 profile_conversations = [
     ("LC", "Люблю поболтать!"),
@@ -22,16 +23,17 @@ profile_animals = [
     ('IA', 'Зависит от животного'),
     ('NA', 'Предпочитаю поездки без животных'),
 ]
+
+
 class Profile(models.Model):
-    profile_photo = models.ImageField(upload_to='upload_to/profile_photos', blank=True, default='user.png',
-                              verbose_name="Изображение")
+    profile_photo = models.ImageField(upload_to='upload_to/profile_photos', blank=True, default='user.jpg',
+                                      verbose_name="Изображение")
     prefer_conversations = models.CharField(max_length=2, choices=profile_conversations, default='IC')
     prefer_music = models.CharField(max_length=2, choices=profile_music, default='IM')
     prefer_smoke = models.CharField(max_length=2, choices=profile_smoke, default='IS')
     prefer_animals = models.CharField(max_length=2, choices=profile_animals, default='IA')
     base_user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Пользователь",
-                             related_name="user_profile")
-
+                                     related_name="user_profile")
 
     def __str__(self):
         return f"{self.base_user.username}"
@@ -40,12 +42,22 @@ class Profile(models.Model):
         verbose_name = "Профиль пользователя"
         verbose_name_plural = "Профили пользователей"
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.profile_photo.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.profile_photo.path)
+
 
 class Message(models.Model):
     sender = models.ForeignKey(Profile, on_delete=models.CASCADE, verbose_name="Отправитель",
-                              related_name='sender')
+                               related_name='sender')
     recipient = models.ForeignKey(Profile, on_delete=models.CASCADE, verbose_name="Получатель",
-                              related_name='recipient')
+                                  related_name='recipient')
     text = models.TextField(verbose_name='Сообщение')
     date = models.DateField(auto_now=True)
     time = models.TimeField(auto_now=True)
@@ -58,6 +70,7 @@ class Message(models.Model):
     class Meta:
         verbose_name = "Сообщение"
         verbose_name_plural = "Сообщения"
+
 
 class Chat(models.Model):
     members = models.ManyToManyField(Profile, verbose_name="Участники")
